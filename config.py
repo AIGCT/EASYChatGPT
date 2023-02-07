6449f3aa5fec265449e1fd5d87341df4d3f784c6
iterhui <767092533@qq.com>
2023-02-07 21:42:03 +0800

    new

--- config.py
+++ config.py
@@ -10,23 +10,18 @@ def getToken():
         configs = json.load(f)
 
     # Get the list of tokens from the JSON object
-    tokens = configs["session_token"]
-    # cf = configs["cf_clearance"]
+    tokens = configs["apikey"]
 
     # Select a random token from the list 并且记录是第几个token
     n = random.randint(0, len(tokens) - 1)
+
     token = tokens[n]
 
-    # ua = configs["user_agent"]
-
-    config = {
-        "session_token": token
-        # "cf_clearance": cf,
-        # "user_agent": ua
-    }
-
-    return config
+    return token
 
 
 if __name__ == "__main__":
-    print(getToken())
+
+    res = getToken()
+    
+    print(res)
